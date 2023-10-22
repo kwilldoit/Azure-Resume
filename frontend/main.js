0d953c7fc43c796c19000ec681437a5ff0d5e1b2
kwilldoit <kirkwilliams361@hotmail.com>
2023-10-22 13:03:18 -0400

    Corrected Main.json - array call to view count value - line 29

--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,40 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
-    getVisitCount()
-
-})
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount().then(count => {
+        if (count !== undefined) {
+            document.getElementById("counter").innerText = count;
+        } else {
+            document.getElementById("counter").innerText = "Data not available";
+        }
+    }).catch(error => {
+        console.error(error);
+    });
+});
 
 const functionApi = 'http://localhost:7071/api/KWCounterTrigger1';
 
-const getVisitCount = () => {
-       let count = 30;
-       fetch(functionApi).then(response => {
-          return response.json()
-       }).then(response =>{
-            console.log("Website caled functionAPI")
-            count = response.count;
-            document.getElementById("counter").innerText = count;
-       }).catch(function(error){
-           console.log(error);
-       });
-       return count; 
+const getVisitCount = async () => {
+    try {
+        const response = await fetch(functionApi);
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        console.log("Website called functionAPI");
+        console.log("Data from API:", data);
+
+        if (data && data.length > 0 && data[0].count !== undefined) {
+            const count = typeof data[0].count === "string"
+                ? parseInt(data[0].count, 10)
+                : data[0].count;
+
+            return count.toString();
+        } else {
+            return "Data not available";
+        }
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 }
